63d57598104fd66dafb43b4e0948abc431702169
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2024-12-10 10:28:32 +0200

    Add logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className="div div">
+            <div className="div">
+                <span>Iron</span>
+                <span>Coding</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
