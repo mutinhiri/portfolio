15b2c81502b73a487c47fdf20927156e9e420587
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2024-12-10 10:30:47 +0200

    Add list item tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,15 @@ function Navbar() {
                 <span>Iron</span>
                 <span>Coding</span>
             </div>
+            <div className="abot">
+                <ul>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+
+                </ul>
+            </div>
         </div>
     </div>
   )
