efd5cf741206b97ad85323f3b118120b10e07703
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2024-12-10 10:57:25 +0200

    Add styling Navbar medium screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar() {
   return (
     <div>
         <div className="container mx-auto hidden md:flex justify-between items-center py-6">
-            <div className="text-xl capitalize lg:text-2xl font-bold flex items-center gap-1">
+            <div className="text-xl lg:text-2xl font-bold flex items-center gap-1">
                 <span className='text-white bg-red-400'>Iron</span>
                 <span className='text-purple-500'>Coding</span>
             </div>
             <div className="abot">
-                <ul className='hidden md:flex'>
+                <ul className='hidden md:flex items-center space-x-6 list-none lg:text-lg md:text-base text-white'>
                     <li>About</li>
                     <li>Services</li>
                     <li>Work</li>
