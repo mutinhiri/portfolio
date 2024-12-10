bf0d2505322af9bbdadd573f54c53c5488f7b4d4
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2024-12-10 10:54:22 +0200

    Add Navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
+    <div className='bg-[#e0e0c1e]'>
+      <Navbar/>
       <h1 className='bg-blue-900 rounded-lg'>Hello</h1>
-    </>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-        <div className="div div">
-            <div className="div">
-                <span>Iron</span>
-                <span>Coding</span>
+        <div className="container mx-auto hidden md:flex justify-between items-center py-6">
+            <div className="text-xl capitalize lg:text-2xl font-bold flex items-center gap-1">
+                <span className='text-white bg-red-400'>Iron</span>
+                <span className='text-purple-500'>Coding</span>
             </div>
             <div className="abot">
-                <ul>
+                <ul className='hidden md:flex'>
                     <li>About</li>
                     <li>Services</li>
                     <li>Work</li>
