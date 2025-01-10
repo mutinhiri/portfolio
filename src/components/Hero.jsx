50ab68fd2877c1d9baa0085fdcbf0da5571a9864
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-10 19:43:03 +0200

    Display text on image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
       className='mx-auto w-2/3 md:w-1/3 lg:w-1/4'
       src={avatar}/>
       <div className='container mx-auto text-center'>
-        <h1 className='text-4xl md:text-5xl flex'>Your Story , Your Way</h1>
-        <span>Build Your Personal Portfolio</span>
+        <h1 className='text-4xl md:text-5xl flex flex-col gap-4 font-bold mb-4'>Your Story , Your Way</h1>
+        <span className='text-purple-500'>Build Your Personal Portfolio</span>
         <p>Showcase your Journey through a personal portfolio</p>
         <button>Hire Me</button>
         <button>My Story</button>
