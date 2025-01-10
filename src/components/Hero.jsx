ce1659858dedf1655a8c2f0e987f762e58028557
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-10 20:04:11 +0200

    Animate the heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,12 +18,14 @@ const Hero = () => {
       initial={{ opacity:0 }}
       animate={{opacity:1}}
       transition= {{
-        duration: 0.7
+        duration: 0.5
       }}
        className='container mx-auto text-center'>
-        <h1 className='text-4xl md:text-5xl flex flex-col gap-4 font-bold mb-4'>Your Story , Your Way
+        <motion.h1
+        initial={{ opacity: 1, y:100 }}
+         className='text-4xl md:text-5xl flex flex-col gap-4 font-bold mb-4'>Your Story , Your Way
         <span className='text-purple-500'>Build Your Personal Portfolio</span>
-        </h1>
+        </motion.h1>
         <p className='text-gray-400 text-lg mb-8'>Showcase your Journey through a personal portfolio</p>
         <div className="flex justify-center space-x-4 mb-10">
           <button className='bg-purple-500 text-white px-6 py-3 rounded-full'>Hire Me</button>
