e2c9444908f41e357472eb280b13cfc1b2ecdab2
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-10 19:48:44 +0200

    Add styling to Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,15 @@ const Hero = () => {
   return (
     <div className='text-white py-10'>
       <img 
-      className='mx-auto w-2/3 md:w-1/3 lg:w-1/4'
+      className='mx-auto w-2/3 md:w-1/3 lg:w-1/4 '
       src={avatar}/>
       <div className='container mx-auto text-center'>
-        <h1 className='text-4xl md:text-5xl flex flex-col gap-4 font-bold mb-4'>Your Story , Your Way</h1>
+        <h1 className='text-4xl md:text-5xl flex flex-col gap-4 font-bold mb-4'>Your Story , Your Way
         <span className='text-purple-500'>Build Your Personal Portfolio</span>
-        <p>Showcase your Journey through a personal portfolio</p>
-        <button>Hire Me</button>
-        <button>My Story</button>
+        </h1>
+        <p className='text-gray-400 text-lg mb-8'>Showcase your Journey through a personal portfolio</p>
+        <button className='bg-purple-500 text-white px-6 py-3 rounded-full'>Hire Me</button>
+        <button className='text-white border border-white px-6 py-3 rounded-full'>My Story</button>
       </div>
         
     </div>
