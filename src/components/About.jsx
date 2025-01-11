74f4c68d2f1581ba2bd9a00cf3e08ea3a85bd073
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-11 07:29:49 +0200

    Add abot me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid illum quas facere labore omnis iure maiores, officia ducimus officiis, sit quia quisquam reiciendis quis. Minus quibusdam magnam veritatis tenetur.
+        </p>
+        <div>
+          <div>
+            <img/>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi laborum laboriosam omnis dicta, necessitatibus ipsa suscipit minima officiis ipsum ipsam voluptate debitis magni repellendus nemo.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
