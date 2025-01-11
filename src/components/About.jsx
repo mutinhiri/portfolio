635a36a9fff953a9f820e5a364c84b470c103b78
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-11 17:58:15 +0200

    Transition from Y axis

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -52,10 +52,10 @@ const About = () => {
             ref={ref}
             initial={{opacity:0 }}
             animate={inView ? {opacity:1 }: {}}
-            transition={{delay:1, duration:0.5}} 
+            transition={{delay:1.2 , duration:0.3}} 
             className='text-center'>
             <h3 className='text-3xl md:text-8xl font-bold md:my-6'> 50 + </h3>
-            <p className='text-sm sm:text-base text-gray-300'>Overall Global Customers</p>
+            <motion.p className='text-sm sm:text-base text-gray-300'>Overall Global Customers</motion.p>
           </motion.div>
 
           <div>
