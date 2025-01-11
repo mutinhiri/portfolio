b39c8b9a645dfdbc9b82f30c1e2745770d38d9f7
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-11 17:48:58 +0200

    Animate scroll view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,21 +13,27 @@ const About = () => {
     <div className='text-white py-16'>
       <div className='container mx-auto px-4 text-center'>
         <motion.h2
-        ref={ref}
-        initial={{opacity:0, y:100}}
-        animate={inView ? {opacity:1, y:0 }: {}}
-        transition={{delay: 0.3, duration:0.5}}
-         className='text-3xl md:text-4xl font-bold mb-8 underline'>About Me</motion.h2>
-        <p className='mb-12 text-gray-400 text-center'>
+          ref={ref}
+          initial={{opacity:0, y:100}}
+          animate={inView ? {opacity:1, y:0 }: {}}
+          transition={{delay: 0.3, duration:0.5}}
+          className='text-3xl md:text-4xl font-bold mb-8 underline'>About Me</motion.h2>
+        <motion.p 
+           ref={ref}
+           initial={{opacity:0, y:100}}
+           animate={inView ? {opacity:1, y:0 }: {}}
+           transition={{delay: 0.5, duration:0.5}}
+           className='mb-12 text-gray-400 text-center'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid illum quas facere labore omnis iure maiores, officia ducimus officiis, sit quia quisquam reiciendis quis. Minus quibusdam magnam veritatis tenetur.
-        </p>
+        </motion.p>
         <div className='flex flex-col md:flex-row justify-center items-center'>
-          <div className='mb-8 md:mb-0 md:mr-8 flex justify-center'>
+          <motion.div 
+            className='mb-8 md:mb-0 md:mr-8 flex justify-center'>
             <img
             src={aboutImg}
             className='w-2/3 sm:w-1/2 md:w-10/12'
             />
-          </div>
+          </motion.div>
           <p
           className='md:w-1/2 text-gray-400 px-4 md:px-0 text-base sm:text-lg md:text-xl'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi laborum laboriosam omnis dicta, necessitatibus ipsa suscipit minima officiis ipsum ipsam voluptate debitis magni repellendus nemo.
