7037acf569e2c164b7ae711443c996ad74a453c8
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 01:24:34 +0200

    Add services component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/About'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Services from './components/Services'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
 
     </div>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Services = () => {
+  const serviceData = [
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    }
+  ]
   return (
     <div>
-        Services
+        <div>
+          <h2>Sevrices</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!</p>
+          <div>
+
+          </div>
+        </div>
     </div>
   )
 }
