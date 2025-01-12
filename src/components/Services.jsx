81881f9b9bd87a5ad6174f0eb8aec25cfe08e2a8
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 01:31:58 +0200

    Addservices tab

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { FaAppStore } from 'react-icons/fa'
+import { FaAppStore } from 'react-icons/fa'
 
 const Services = () => {
   const serviceData = [
     {
       title: "App Design",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
-    }
+    },
+
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    },
+
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    },
+
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    },
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    },
+    {
+      title: "App Design",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!"
+    },
   ]
   return (
     <div>
@@ -13,7 +38,15 @@ const Services = () => {
           <h2>Sevrices</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!</p>
           <div>
-
+            {
+              serviceData.map((service, index) =>(
+                <div
+                  key={index}>
+                    <FaAppStore/>
+                    <h3>{service.title}</h3>
+                  </div>
+              ))
+            }
           </div>
         </div>
     </div>
