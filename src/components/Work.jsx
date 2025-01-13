940f1e63d42f31af008c6048d43ed0958441aad0
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 02:26:58 +0200

    Add projects array

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Work = () => {
+
+    const projects = [
+        {
+            id:11,
+            title: "Text toitle",
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non totam, nisi dolore voluptas facilis sunt in.",
+            image: ""
+        }
+    ]
   return (
     <div>
-        
+        <div>
+            <h2>My Work</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod hic modi officiis at expedita tenetur.
+            </p>
+            <div>
+
+            </div>
+        </div>
     </div>
   )
 }
