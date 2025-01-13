d76ab7516cb5a52d374c2967becd1d50d42f1f51
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 03:52:49 +0200

    Add scroll variants

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -11,12 +11,42 @@ const Companies = () => {
 
     const companiesList = [...companies, ...companies, ...companies, ...companies]
 
+    const scrollVariants = {
+        animate: {
+            x: [0, -2000],
+            transition: {
+                x: {
+                    repeat: Infinity,
+                    repeatType: "loop",
+                    duration: 15,
+                    ease: "linear",
+
+                },
+            },
+        },
+    };
+
+    const scrollVariants2 = {
+        animate: {
+            x: [0, -2000],
+            transition: {
+                x: {
+                    repeat: Infinity,
+                    repeatType: "loop",
+                    duration: 15,
+                    ease: "linear",
+
+                },
+            },
+        },
+    };
+
   return (
     <div className='text-white py-16'>
         <div className='container mx-auto text-center'>
             <h2 className='text-3xl md:text-4xl font-bold mb-8'>Companies I have worked with</h2>
             <div className='overflow-hidden relative w-full'>
-                <div className='whitespace-nowrap flex space-x-10 '>
+                <motion.div className='whitespace-nowrap flex space-x-10 '>
                     {
                         companiesList.map((company, index) => (
                             <div
@@ -26,7 +56,7 @@ const Companies = () => {
                             </div>
                         ))
                     }
-                </div>
+                </motion.div>
                 
             </div>
             <div className='overflow-hidden relative w-full mt-5'>
@@ -34,6 +64,7 @@ const Companies = () => {
                     {
                         companiesList.map((company, index) => (
                             <div
+                            className='text-lg bg-gray-800 px-6 py-3 rounded-full inline-block'
                             key={index}>
                                 {company}
                             </div>
