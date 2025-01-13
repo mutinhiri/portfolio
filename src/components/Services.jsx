2ce7642cf56e1c8203433f6e87f4b3317574b9d8
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 02:05:06 +0200

    Add bollean for scale

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -40,27 +40,36 @@ const Services = () => {
     triggerOnce: true,
     threshold: 0.2
   })
-  
+
   return (
-    <div className='text-white py16'>
+    <motion.div 
+    ref={ref}
+    initial={{opacity: 0, y:50}}
+    animate={inView ? {opacity: 1, y:0 } : {}}
+    transition={{duration: 0.5}}
+    className='text-white py16'>
         <div className='container mx-auto px-4 text-center'>
           <h2 className='text-3xl md:text-4xl font-bold underline mb-8'>Services</h2>
           <p className='mb-12 text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, deleniti!</p>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
             {
               serviceData.map((service, index) =>(
-                <div
+                <motion.div
+                  ref={ref}
+                  initial={{opacity: 0, scale: 0.8}}
+                  animate={inView ? {opacity: 1, scale:1 } : {opacity: 0, scale: 0.8}}
+                  transition={{duration: 0.5}}
                   key={index}
                   className='bg-[#1c1a2b] rounded-lg p-6 text-center hover:shadow-lg hover:shadow-purple-500 transition-shadow duration-300'>
                     <FaAppStore className='text-purple-500 text-4xl sm:text-5xl lg:text-6xl mb-4 mx-auto'/>
                     <h3 className='text-lg sm:text-xl lg:text-2xl font-semibold mb-2'>{service.title}</h3>
                     <p className='text-sm sm:text-base lg:text-lg text-gray-400'>{service.description}</p>
-                  </div>
+                  </motion.div>
               ))
             }
           </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
