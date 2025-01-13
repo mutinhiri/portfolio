bc94e4b40ece7f87e900e3d2cd3f9768fa03e2f9
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 03:29:26 +0200

    Add header element

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -4,12 +4,16 @@ import { motion } from 'motion/react'
 const Companies = () => {
     const companies = [
         "gooh",
-        "jkllo"
+        "jkllo",
+        "jjjjkk",
+        "hdjjjkwk"
     ]
-    
+
   return (
     <div>
-
+        <div>
+            <h2>Companies I have worked with</h2>
+        </div>
     </div>
   )
 }
