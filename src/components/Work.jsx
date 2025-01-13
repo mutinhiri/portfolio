ae3165d366bc5637f492e9d8fc1bd31bbc4638ee
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 12:55:06 +0200

    Add contacts page

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -73,9 +73,14 @@ const Work = () => {
                 }
             </div>
         </div>
-        <div>
+        <motion.div
+        ref={ref}
+        initial={{opacity:0, y:100 }}
+        animate={inView ? {opacity:1, y:0} : {}}
+        transition={{delay:0.7, duration:0.5}}
+        >
             <Companies/>
-        </div>
+        </motion.div>
     </div>
   )
 }
