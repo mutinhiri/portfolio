1087738e10ce3e875232d5dc82640885398720ed
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-01-13 03:28:27 +0200

    Animate companies display

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { motion } from 'motion/react'
 
 const Companies = () => {
+    const companies = [
+        "gooh",
+        "jkllo"
+    ]
+    
   return (
     <div>
-        
+
     </div>
   )
 }
